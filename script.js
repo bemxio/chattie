466452561522bf6b9e4d20d334ab10e0f3e4c493
heyngra <heyngra.wspolpraca@gmail.com>
2022-10-14 18:34:43 +0200

    add ws:// autocompletion

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const messages = document.getElementById("message-list");
 const inputBox = document.getElementById("input-box");
-
+const wsorwss = window.location.href.startsWith("https://") ? "wss://" : "ws://"
 const params = new URLSearchParams(window.location.search);
 
 let ip = params.has("ip") ? params.get("ip") : prompt("Please enter the IP address of a server:");
+ip = (ip.startsWith("ws://")||ip.startsWith("wss://")) ? ip : (ip.includes("://") ? wsorwss+ip.split("://")[ip.split("://").length-1] : wsorwss+ip) // fix if IP is not websocket url
 let name = params.has("name") ? params.get("name") : prompt("Please enter your username:");
 
 const socket = new WebSocket(ip);
